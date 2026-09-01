3f7b8084419509c66b833d17ac315866368e1a84
Joviank <Joviak@hotmail.no>
2026-09-01 11:22:36 +0200

    Økt 2 uke 1: Satt opp tester for to-do liste, laget modell og satt opp service

---
+++ TaskService.tests/ToDoListTests.cs
@@ -0,0 +1,53 @@
+﻿namespace TaskService.tests;
+
+public class TaskServiceTests
+{
+    [Fact]
+    public void AddTask_ShouldAddTaskToList()
+    {
+        // Arrange
+        var service = new TaskService();
+
+        // Act
+        service.AddTask("Avskaffe monarkiet");
+
+        // Assert
+        Assert.Single(service.GetTasks());
+    }
+
+    [Fact]
+    public void AddTask_ShouldStoreCorrectTitle()
+    {
+        // Given
+        var service = new TaskService();
+        // When
+        service.AddTask("Avskaffe monarkiet part 2");
+    
+        // Then
+        var task = service.GetTasks().First();
+        Assert.Equal("Avskaffe monarkiet part 2", task.Title);
+    }
+
+    [Fact]
+    public void CompleteTask_ShouldMarkAsCompleted()
+    {
+        // Arrange
+        var service = new TaskService();
+        var task = service.AddTask("Avskaffe monarkiet part 3");
+
+        // Act 
+        service.CompleteTask(task.Id);
+
+        // Assert
+        Assert.True(service.GetTasks().First().IsCompleted);
+    }
+    [Fact]
+    public void CompleteTask_ShouldThrow_WhenTaskDoesntExist()
+    {
+        // Arrange
+        var service = new TaskService();
+
+        // Act & Assert
+        Assert.Throws<KeyNotFoundException>(() => service.CompleteTask(1846374));
+    }
+}
---
+++ TaskService/TaskService.cs
@@ -0,0 +1,30 @@
+﻿namespace TaskService;
+
+public class TaskService
+{
+    private readonly List<TaskItem> tasks = new();
+    private int nextId = 0;
+
+    public TaskItem AddTask(string title)
+    {
+        var task = new TaskItem(nextId++, title);
+        tasks.Add (task);
+        return task;
+    }
+
+    public List<TaskItem> GetTasks()
+    {
+        return tasks.ToList();
+    }
+
+    public void CompleteTask(int id)
+    {
+        var task = tasks.FirstOrDefault(t => t.Id == id);
+
+        if(task == null)
+        {
+            throw new KeyNotFoundException("Du vil ha monarkiet. (Tasken finnes ikke)");
+        }
+        task.IsCompleted = true;
+    }
+}
